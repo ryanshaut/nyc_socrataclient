e07313e22b8ffa85cd46cb6d6b8ee80689f5a27c
it-admin <it-admin@deb10-python.hq.shaut.us>
2021-08-01 19:24:17 -0400

    initial commit

--- src/api.py
+++ src/api.py
@@ -1,8 +1,8 @@
-import os, time, uuid,json
+import os, time,json
 
-from requests.models import HTTPError, ReadTimeoutError
+from requests.models import HTTPError, ReadTimeoutError, Response
 from sodapy import Socrata
-from utils import log, logResults
+from utils import fileExists, loadJSONFile, log, logResults, saveFile, stripChars
 from requests import ReadTimeout
 
 
@@ -14,35 +14,109 @@ socrata_timeout = int(os.environ.get('socrata_timeout'))
 showExecutionTimes = os.environ.get('showExecutionTimes')
 
 
+class APIResponse():
+    def __init__(self) -> None:
+        self.response = None
+        self.error = None
+        self.isOK = False
+        self.fromCachedResponse = False
+    
+    def save(self, fileName=None):
+        if fileName is None:
+            raise 'fileName not provided!'
+        saveFile(fileName, self.response)
+
+
+class Api():
+    def __init__(self):
+        self.client = self.getConnection()
+
+    def getConnection(self):
+        log('Getting a connection to socrata')
+        return Socrata(socrata_domain, socrata_token,timeout=socrata_timeout)
+
+
+    def query(self, **kwargs):
+        # log("Executing query: %s" % (json.dumps([kwargs])))
+        if showExecutionTimes == 'True':
+            start = time.time()
+        global client
+        res = APIResponse()
+
+        cachedFile = None
+        if ('query' in kwargs):
+            queryName  = stripChars(kwargs.get('query'))
+            cachedFile = 'results/%s.json' % queryName
+        
+        if fileExists(cachedFile):
+            # log("using cached file")
+            res.response = loadJSONFile(cachedFile)
+            res.isOK = True
+            res.fromCachedResponse = True
+            return res
+        else:
+            try:
+                data = self.client.get(socrata_dataset_identifier,**kwargs)
+                res.response = data
+                # log("Saving results")
+                res.isOK = True
+            except (ReadTimeout,ReadTimeoutError) as e:
+                msg = "Timeout while fetching the data, either optimize the query, or try a higher timeout when connecting"
+                log(msg, 'ERROR')
+                res.error = msg
+            except HTTPError as e:
+                res.error = e
+                log("HTTP Error! %s" % str(str(e).split(":")[0]))
+            except Exception as e:
+                res.error = e
+                log("Unexpected Error! %s" % str(str(e).split(":")[0]))
+            
+            if showExecutionTimes == 'True':
+                end = time.time()
+                # log("API call done in %d seconds" % (round(end - start, 2)))
+            if res.response is not None:
+                res.save(cachedFile)
+                return res
+            else:
+                return res
+
+
+
+
+
+
+
+
 
 def getConnection():
     return Socrata(socrata_domain, socrata_token,timeout=socrata_timeout)
 
 
-def query(**kwargs):
-    log("Executing query: %s" % (json.dumps([kwargs])))
-    if showExecutionTimes == 'True':
-        start = time.time()
-    global client
-    try:
-        result = client.get(socrata_dataset_identifier,**kwargs)
+# def query(**kwargs):
+#     log("Executing query: %s" % (json.dumps([kwargs])))
+#     if showExecutionTimes == 'True':
+#         start = time.time()
+#     global client
 
-        log("Saving results")
-        logResults(json.dumps(kwargs), json.dumps(result))
-    except (ReadTimeout,ReadTimeoutError) as e:
-        msg = "Timeout while fetching the data, either optimize the query, or try a higher timeout when connecting"
-        log(msg, 'ERROR')
-        result = msg
-    except HTTPError as e:
-        result = e
-        log("HTTP Error! %s" % str(str(e).split(":")[0]))
-    except Exception as e:
-        result = e
-        log("Unexpected Error! %s" % str(str(e).split(":")[0]))
+#     res = APIResponse()
+#     try:
+#         res.response = client.get(,**kwargs)
+#         log("Saving results")
+#         logResults(json.dumps(kwargs), json.dumps(res.response))
+#     except (ReadTimeout,ReadTimeoutError) as e:
+#         msg = "Timeout while fetching the data, either optimize the query, or try a higher timeout when connecting"
+#         log(msg, 'ERROR')
+#         res.error = msg
+#     except HTTPError as e:
+#         res.error = e
+#         log("HTTP Error! %s" % str(str(e).split(":")[0]))
+#     except Exception as e:
+#         res.error = e
+#         log("Unexpected Error! %s" % str(str(e).split(":")[0]))
     
-    if showExecutionTimes == 'True':
-        end = time.time()
-        log("API call done in %d seconds" % (round(end - start, 2)))
-    return result
+#     if showExecutionTimes == 'True':
+#         end = time.time()
+#         log("API call done in %d seconds" % (round(end - start, 2)))
+#     return res
 
 client = getConnection()
--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,17 @@
 import json
 import time
 import uuid
+import datetime
+import re
+from os import path
+import random
 
 def prettyJson(data):
     if type(data) == type("string"):
         parsed = json.loads(data)
     if type(data) == type({}): # dict
+        parsed = data
+    else:
         parsed = data
     return (json.dumps(parsed, indent=4, sort_keys=True))
 
@@ -19,20 +25,56 @@ def log(msg=None, level=None):
     print(message)
     return message
 
+def fileExists(fileName) -> bool:
+    return path.exists(fileName)
+
 def logResults(query, data):
     randomString = str(uuid.uuid4())[0:8]
     queryFile = "results/%s_query.json" % randomString
     dataFile = "results/%s_data.json" % randomString
-    with open(queryFile, 'w') as outfile:
-        outfile.writelines(prettyJson(query))
-    with open(dataFile, 'w') as outfile:
-        outfile.writelines(prettyJson(data))
+    saveFile(queryFile, query)
+    saveFile(dataFile, data)
+
+def saveFile(file, data):
+    with open(file, 'w') as outfile:
+        outfile.writelines(json.dumps(data))
+
+def loadFile(file):
+    if fileExists(file):
+        try:
+            with open(file,'r') as infile:
+                data = infile.readlines()
+            return data
+        except Exception as e:
+            return None
+    return None
 
 def loadJSONFile(path):
     return json.load(open(path, 'r'))
     # with open(path,'r') as iFile:
     #     data = iFile.readlines()
     # return json.loads(data[0])
+
+def stripChars(text) -> str:
+    return re.sub('\W','',text)
+
+# rounds to the nearest second
+def timeDelta(start, end, fmt, output):
+    s = datetime.datetime.strptime(start, fmt)
+    e = datetime.datetime.strptime(end, fmt)
+    deltaSeconds =  round((e - s).total_seconds())
+    if output:
+        if output.lower() == 'seconds':
+            return deltaSeconds
+        if output.lower() == 'minutes':
+            return deltaSeconds / 60
+        if output.lower() == 'hours':
+            return deltaSeconds / 60 / 60
+        if output.lower() == 'minutes':
+            return deltaSeconds / 60 / 24
+        raise "%s is not an output type!" % output
+    else:
+        return deltaSeconds
 
 
 # input JSON list of objects
@@ -60,3 +102,19 @@ def extractFromJSON(data=None,attribs=None):
             if k in attribs:
                 output[k].append(row[k])
     return output
+
+
+def toDate(date,datefmt):
+    return datetime.datetime.strptime(date, datefmt)
+
+def getDOWFromDate(date, datefmt):
+    try:
+        return toDate(date, datefmt).strftime('%a')
+    except Exception as e:
+        return None
+
+
+def random_color():
+    rgbl=[255,0,0]
+    random.shuffle(rgbl)
+    return tuple(rgbl)
